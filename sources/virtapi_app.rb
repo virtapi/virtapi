2a4d90460751f443ab4522bbe3a35c66a1edfb37
Tim Meusel <Tim Meusel>
2016-01-06 00:50:38 +0100

    use the rails config directory for database config
    this:
    * matches the Rails guidelines
    * supports prod + test + testing env

--- sources/virtapi_app.rb
+++ sources/virtapi_app.rb
@@ -8,8 +8,13 @@ require 'active_record'
 
 Dir.glob('./{models,controllers}/*.rb').each { |file| require file }
 
+@environment = ENV['RACK_ENV'] || 'development'
+@dbconfig = YAML.load(File.read('config/database.yml'))
 ActiveRecord::Base.logger = Logger.new(STDERR)
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: "#{__dir__}/dbfile.sqlite")
+ActiveRecord::Base.establish_connection @dbconfig[@environment]
+
+# manual style
+#ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: "#{__dir__}/dbfile.sqlite")
 
 before /.*/ do
   if request.path_info.match(/.json$/)

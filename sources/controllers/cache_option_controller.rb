1831924d04fa7e285125a73e7461411fb9615ddd
Tim Meusel <Tim Meusel>
2016-01-07 23:18:52 +0100

    add cache_option controller to meet our CRUD style

---
+++ sources/controllers/cache_option_controller.rb
@@ -0,0 +1,29 @@
+namespace '/cache_options' do
+  get do
+    @cache_options = CacheOption.all()
+    json :cache_option => @cache_options
+  end
+
+  post do
+    CacheOption.create!(params[:cache_option])
+  end
+
+  before %r{\A/(?<id>\d+)/?.*} do
+    @cache_option = CacheOption.find(params[:id])
+  end
+
+  namespace '/:id' do
+    delete do
+      @cache_option.delete
+    end
+
+    patch do
+      @cache_option.assign_attributes(params[:cache_option]).save!
+      redirect to("/cache_options/#{@cache_option.id}")
+    end
+
+    get do
+      json :cache_option => @cache_options
+    end
+  end
+end

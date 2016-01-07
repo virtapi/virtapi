d5429b0c06bad3e04609e646c735ada7410601e8
Tim Meusel <Tim Meusel>
2016-01-07 23:44:53 +0100

    add vlan controller

---
+++ sources/controllers/vlan_controller.rb
@@ -0,0 +1,29 @@
+namespace '/vlans' do
+  get do
+    @vlans = Vlan.all()
+    json :vlan => @vlans
+  end
+
+  post do
+    Vlan.create!(params[:vlan])
+  end
+
+  before %r{\A/(?<id>\d+)/?.*} do
+    @vlan = Vlan.find(params[:id])
+  end
+
+  namespace '/:id' do
+    delete do
+      @vlan.delete
+    end
+
+    patch do
+      @vlan.assign_attributes(params[:vlan]).save!
+      redirect to("/vlans/#{@vlan.id}")
+    end
+
+    get do
+      json :vlan => @vlan
+    end
+  end
+end

6dfacbee6e262746798b5abc140991d20e51d78f
Tim Meusel <Tim Meusel>
2016-01-07 23:42:51 +0100

    add node_method controller

---
+++ sources/controllers/node_method_controller.rb
@@ -0,0 +1,29 @@
+namespace '/node_methods' do
+  get do
+    @node_methods = NodeMethod.all()
+    json :node_method => @node_methods
+  end
+
+  post do
+    NodeMethod.create!(params[:node_method])
+  end
+
+  before %r{\A/(?<id>\d+)/?.*} do
+    @node_method = NodeMethod.find(params[:id])
+  end
+
+  namespace '/:id' do
+    delete do
+      @node_method.delete
+    end
+
+    patch do
+      @node_method.assign_attributes(params[:node_method]).save!
+      redirect to("/node_methods/#{@node_method.id}")
+    end
+
+    get do
+      json :node_method => @node_method
+    end
+  end
+end

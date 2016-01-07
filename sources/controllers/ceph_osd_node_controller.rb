3ededd563a48122af08cdaecbf1daf996f284216
Tim Meusel <Tim Meusel>
2016-01-07 23:25:37 +0100

    add ceph_osd_node controller to meet our CRUD style

---
+++ sources/controllers/ceph_osd_node_controller.rb
@@ -0,0 +1,29 @@
+namespace '/ceph_osd_nodes' do
+  get do
+    @ceph_osd_nodes = CephOsdNode.all()
+    json :ceph_osd_node => @ceph_osd_nodes
+  end
+
+  post do
+    CephOsdNode.create!(params[:ceph_osd_node])
+  end
+
+  before %r{\A/(?<id>\d+)/?.*} do
+    @ceph_mon_node = CephOsdNode.find(params[:id])
+  end
+
+  namespace '/:id' do
+    delete do
+      @ceph_osd_node.delete
+    end
+
+    patch do
+      @ceph_osd_node.assign_attributes(params[:ceph_osd_node]).save!
+      redirect to("/ceph_osd_nodes/#{@ceph_osd_node.id}")
+    end
+
+    get do
+      json :cephosdd_node => @ceph_osd_node
+    end
+  end
+end

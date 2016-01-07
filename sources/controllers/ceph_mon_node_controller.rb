014b2004aba7a2c46e15f4529b2e6475cefc6d42
Tim Meusel <Tim Meusel>
2016-01-07 23:21:53 +0100

    add ceph_mon_node controller to meet our CRUD style

---
+++ sources/controllers/ceph_mon_node_controller.rb
@@ -0,0 +1,29 @@
+namespace '/ceph_mon_nodes' do
+  get do
+    @ceph_mon_nodes = CephMonNode.all()
+    json :ceph_mon_node => @ceph_mon_nodes
+  end
+
+  post do
+    Domain.create!(params[:ceph_mon_node])
+  end
+
+  before %r{\A/(?<id>\d+)/?.*} do
+    @ceph_mon_node = CephMonNode.find(params[:id])
+  end
+
+  namespace '/:id' do
+    delete do
+      @ceph_mon_node.delete
+    end
+
+    patch do
+      @ceph_mon_node.assign_attributes(params[:ceph_mon_node]).save!
+      redirect to("/ceph_mon_nodes/#{@ceph_mon_node.id}")
+    end
+
+    get do
+      json :ceph_mon_node => @ceph_mon_node
+    end
+  end
+end

b135eaabe460057089fb3f85bdd01aa1a0adb196
Flipez <github@flipez.net>
2016-01-13 22:32:58 +0000

    move clazz handling outside of respond_to

--- sources/lib/generic_helper.rb
+++ sources/lib/generic_helper.rb
@@ -1,13 +1,13 @@
 def return_resource object: nil
   if object
+    clazz = object.class.to_s
+    clazz = if clazz[/([\S]+)::/]
+              clazz = /([\S]+)::/.match(clazz)[1].pluralize
+            else
+              clazz
+            end.underscore
+    
     respond_to do |type|
-      clazz = object.class.to_s
-      clazz = if clazz[/([\S]+)::/]
-                clazz = /([\S]+)::/.match(clazz)[1].pluralize
-              else
-                clazz
-              end.underscore
-      
       type.html do
         haml clazz.to_sym
       end

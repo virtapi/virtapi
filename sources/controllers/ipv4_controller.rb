eed3c0d9820132b10b2d5273e3351eea118fc97e
Flipez <github@flipez.net>
2016-01-13 21:13:26 +0000

    enhance ipv4

--- sources/controllers/ipv4_controller.rb
+++ sources/controllers/ipv4_controller.rb
@@ -1,11 +1,11 @@
 namespace '/ipv4s' do
   get do
     @ipv4s = Ipv4.all()
-    json :ipv4 => @ipv4s
+    json ipv4: @ipv4s
   end
 
   post do
-    Ipv4.create!(params[:ipv4])
+    json ipv4: Ipv4.create!(params[:ipv4])
   end
 
   before %r{\A/(?<id>\d+)/?.*} do
@@ -14,16 +14,23 @@ namespace '/ipv4s' do
 
   namespace '/:id' do
     delete do
-      @ipv4.delete
+      json ipv4: @ipv4.delete
     end
 
     patch do
       @ipv4.assign_attributes(params[:ipv4]).save!
-      json :ipv4 => @ipv4
+      resource
     end
 
     get do
-      json :ipv4 => @ipv4
+      resource
     end
   end
+
+private
+
+  def resource as_json: true
+    as_json ? json(ipv4: @ipv4) : @ipv4
+  end
+
 end

7b6983ee25f65b974d9abbb0b40006f197fd8fee
Flipez <github@flipez.net>
2016-01-13 22:29:46 +0000

    render haml

--- sources/lib/generic_helper.rb
+++ sources/lib/generic_helper.rb
@@ -1,18 +1,18 @@
 def return_resource object: nil
   if object
     respond_to do |type|
+      clazz = object.class.to_s
+      clazz = if clazz[/([\S]+)::/]
+                clazz = /([\S]+)::/.match(clazz)[1].pluralize
+              else
+                clazz
+              end.underscore
       
       type.html do
-        'enter view code here'
+        haml clazz.to_sym
       end
       
       type.json do
-        clazz = object.class.to_s.downcase
-        clazz = if clazz[/([\S]+)::/]
-                  clazz = /([\S]+)::/.match(clazz)[1].pluralize
-                else
-                  clazz
-                end
         json clazz => object
       end
     
